35d441c0b1a6fe0ff82fa1f3753487a8e664eecb
minchoco <minchocosong@gmail.com>
2020-05-07 19:03:29 +0900

    konlpy 파일 업로드

---
+++ jupdaeri-crawler/ko_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from konlpy.tag import Kkma
+from konlpy.tag import Komoran
+from konlpy.tag import Hannanum
+from konlpy.tag import Mecab
+from konlpy.tag import Okt # Twitter  형태소분석기
+
+#형태소 분석을 위한 객체 생성
+kkma = Kkma()
+Komoran = Komoran()
+hannanum = Hannanum()
+mecab = Mecab()
+okt = Okt()
